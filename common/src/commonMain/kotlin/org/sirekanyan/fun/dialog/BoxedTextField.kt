53304a3497a8c6add00c86af512bb715efdd5e17
Vadik Sirekanyan <me@sirekanyan.org>
2023-09-12 18:06:09 +0400

    Added basic text field

--- common/src/commonMain/kotlin/org/sirekanyan/fun/dialog/BoxedTextField.kt
+++ common/src/commonMain/kotlin/org/sirekanyan/fun/dialog/BoxedTextField.kt
@@ -3,15 +3,16 @@ package org.sirekanyan.`fun`.dialog
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import org.sirekanyan.`fun`.imePadding
@@ -20,16 +21,16 @@ import org.sirekanyan.`fun`.imePadding
 fun BoxedTextField(value: TextFieldValue, onValueChange: (TextFieldValue) -> Unit, readOnly: Boolean) {
     val focusRequester = remember { FocusRequester() }
     Box(Modifier.fillMaxSize().padding(24.dp)) {
-        @OptIn(ExperimentalMaterial3Api::class)
-        OutlinedTextField(
+        BasicTextField(
             value = value,
             onValueChange = onValueChange,
             modifier = Modifier
                 .fillMaxSize()
                 .imePadding()
                 .focusRequester(focusRequester),
-            placeholder = { Text("Put something here...") },
             readOnly = readOnly,
+            textStyle = MaterialTheme.typography.bodyLarge.copy(LocalContentColor.current),
+            cursorBrush = SolidColor(MaterialTheme.colorScheme.primary),
         )
     }
     LaunchedEffect(Unit) {
