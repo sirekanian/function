de2b6e2467622d759cb8768a6cd904ede6c6e54f
Vadik Sirekanyan <me@sirekanyan.org>
2023-09-13 21:33:21 +0400

    Added text field border

--- common/src/commonMain/kotlin/org/sirekanyan/fun/dialog/EditContent.kt
+++ common/src/commonMain/kotlin/org/sirekanyan/fun/dialog/EditContent.kt
@@ -1,6 +1,7 @@
 package org.sirekanyan.`fun`.dialog
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.border
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
@@ -11,13 +12,16 @@ import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 import org.sirekanyan.`fun`.BackHandler
 import org.sirekanyan.`fun`.appbar.SmallToolbar
 import org.sirekanyan.`fun`.data.FunRepository
+import org.sirekanyan.`fun`.imePadding
 import org.sirekanyan.`fun`.model.AppState
 import org.sirekanyan.`fun`.model.EditScreen
 import org.sirekanyan.`fun`.model.HomeScreen
@@ -34,9 +38,12 @@ fun EditContent(state: AppState, screen: EditScreen, repository: FunRepository) 
         state.screen = HomeScreen
     }
     val haptics = LocalHapticFeedback.current
+    val borderColor = if (screen.readOnly) Color.Transparent else MaterialTheme.colorScheme.primary
     Box(
         modifier = @OptIn(ExperimentalFoundationApi::class) Modifier
             .fillMaxSize()
+            .imePadding()
+            .border(1.dp, borderColor, MaterialTheme.shapes.large)
             .combinedClickable(
                 interactionSource = remember { MutableInteractionSource() },
                 indication = null,
