a4ef807caf90ad8e8325bbf365bc72abb9b189d6
Vadik Sirekanyan <me@sirekanyan.org>
2023-09-14 00:04:00 +0400

    Added transparency for horizontal app bar

--- common/src/commonMain/kotlin/org/sirekanyan/fun/appbar/HorizontalAppBar.kt
+++ common/src/commonMain/kotlin/org/sirekanyan/fun/appbar/HorizontalAppBar.kt
@@ -1,5 +1,6 @@
 package org.sirekanyan.`fun`.appbar
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
@@ -7,9 +8,12 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.sirekanyan.`fun`.D
 import org.sirekanyan.`fun`.navigationBarsPadding
 
@@ -18,9 +22,9 @@ fun HorizontalAppBar(
     actions: @Composable RowScope.() -> Unit,
     fab: @Composable () -> Unit,
 ) {
-    AppBarSurface {
         Row(
             modifier = Modifier
+                .background(MaterialTheme.colorScheme.surfaceColorAtElevation(3.dp).copy(alpha = 0.98f))
                 .navigationBarsPadding()
                 .fillMaxWidth()
                 .height(D.appBarSize)
@@ -34,7 +38,6 @@ fun HorizontalAppBar(
                 contentAlignment = Alignment.Center,
             ) {
                 fab()
-            }
         }
     }
 }
--- common/src/commonMain/kotlin/org/sirekanyan/fun/appbar/VerticalAppBar.kt
+++ common/src/commonMain/kotlin/org/sirekanyan/fun/appbar/VerticalAppBar.kt
@@ -1,5 +1,6 @@
 package org.sirekanyan.`fun`.appbar
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
@@ -7,9 +8,12 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.sirekanyan.`fun`.D
 import org.sirekanyan.`fun`.systemBarsPadding
 
@@ -18,9 +22,9 @@ fun VerticalAppBar(
     actions: @Composable ColumnScope.() -> Unit,
     fab: @Composable () -> Unit,
 ) {
-    AppBarSurface {
         Column(
             modifier = Modifier
+                .background(MaterialTheme.colorScheme.surfaceColorAtElevation(3.dp))
                 .fillMaxHeight()
                 .width(D.appBarSize)
                 .systemBarsPadding()
@@ -35,6 +39,5 @@ fun VerticalAppBar(
             }
             Spacer(Modifier.weight(1f))
             actions()
-        }
     }
 }
